015d64926486113b404c0e9b0414237fbc3a211a
Naam's Desktop <noaml12@hotmail.com>
2021-06-02 15:37:58 +0300

    mongo connection test

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
+import os, dotenv, certifi
+from bson.json_util import dumps
 from flask import Flask
+from flask_pymongo import PyMongo
+
+# env_filepath = dotenv.find_dotenv('.env')
+env_filepath = os.path.realpath('.env')
+if os.path.exists(env_filepath):
+    dotenv.load_dotenv(env_filepath)
 
 app = Flask(__name__)
 
+# app.config['MONGO_URI'] = os.environ.get('MONGO_RW_URI')
+app.config['MONGO_URI'] = os.getenv('MONGO_R_URI')
+mongo = PyMongo(app, tlsCAFile=certifi.where())
+
 @app.route('/')
 def index():
-    return '<h1>Hello from Flask</h1>'
+    res = mongo.db['users'].find_one({'username':'admin'})
+    # res = dumps(res['username'])
+    return res['username']
