70e3ed3d0be49eb548d9af19108bd888e5b2ef29
Naam's Desktop <noaml12@hotmail.com>
2022-10-21 11:35:40 +0300

    added CORS middleware

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,19 @@
 import dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # dotenv
 dotenv.load_dotenv()
 
 # fastapi
 app = FastAPI(docs_url='/docs')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 # initialize all necessary common modules and values
 from .common import *
